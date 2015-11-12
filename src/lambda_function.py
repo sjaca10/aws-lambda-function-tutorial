603be70858efeb6c63a5d7025b75d0c724ec6c32
Javier Aguila <sjaca10@gmail.com>
2015-11-12 15:35:56 -0600

    Lambda function example

---
+++ src/lambda_function.py
@@ -0,0 +1,7 @@
+from __future__ import print_function
+
+def lambda_handler(event, context):
+    print('value1 = {}'.format(event['key1']))
+    print('value2 = {}'.format(event['key2']))
+    print('value3 = {}'.format(event['key3']))
+    return event['key1'] # echo first key value
